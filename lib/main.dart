1974eeb2abe57455924b654a285473f47fff798d
YASH2002VERMA <96940635+YASH2002VERMA@users.noreply.github.com>
2023-07-18 23:01:43 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,13 +5,4 @@ void main() {
   runApp(const Quiz());
 }
 
-// class MainApp extends StatelessWidget {
-//   const MainApp({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Quiz();
-//   }
-// }
-
 
